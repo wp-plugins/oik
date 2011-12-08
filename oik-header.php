7f16511bf28beee38ca59a8a760b4737c85d2b22
bobbingwide <bobbingwide@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-12-08 18:07:25 +0000

    version 1.7: correct extraneous header characters, add support for BuddyPress theme in oik-header
    git-svn-id: https://plugins.svn.wordpress.org/oik/trunk@472539 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- oik-header.php
+++ oik-header.php
@@ -41,6 +41,8 @@ add_action( 'wp_footer', 'bw_page_header_style' );
  * Note: The selector is currently coded for Artisteer themes
  * We also assume that the whole of the background image is to be changed.
  * So we turn off the display of the div.art-headerobject as well.
+ * 
+ * Also added support for pages with themes that use div id='header'
  */
 function bw_page_header_style() {
   $post_id = get_the_id();
@@ -50,7 +52,7 @@ function bw_page_header_style() {
     $header_image = $post_meta ;
     e('<style type="text/css">');
     
-    e( "body.postid-$post_id div.art-header, body.page-id-$post_id div.art-header { background-image: url('$header_image'); } " );
+    e( "body.postid-$post_id div.art-header, body.page-id-$post_id div.art-header, body.page-id-$post_id div#header { background-image: url('$header_image'); } " );
     e( "div.art-headerobject { display: none; } ");
     e('</style>');
     echo( bw_ret());
