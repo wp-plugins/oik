e75c0501745afc84ae08fe545e412655ab08cdf4
bobbingwide <bobbingwide@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-04-30 21:10:16 +0000

    version 1.12.1: correct dependency checking for WordPress Multisite installations
    git-svn-id: https://plugins.svn.wordpress.org/oik/trunk@538333 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- oik-sidebar.php
+++ oik-sidebar.php
@@ -4,7 +4,7 @@
 Plugin Name: oik sidebar
 Plugin URI: http://www.oik-plugins.com/oik
 Description: Applies widget wrangler sidebar functionality to Artisteer themes
-Version: 1.12
+Version: 1.12.1
 Author: bobbingwide
 Author URI: http://www.bobbingwide.com
 License: GPL2
