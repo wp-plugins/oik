71c2fe6bec61c507786c131cf5fbd503c64f6a13
bobbingwide <bobbingwide@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-08-07 16:58:08 +0000

    version 1.16: Fix fatal error in oik-header.inc, change oik.css
    git-svn-id: https://plugins.svn.wordpress.org/oik/trunk@582748 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- oik-bob-bing-wide.php
+++ oik-bob-bing-wide.php
@@ -4,7 +4,7 @@
 Plugin Name: oik bob bing wide shortcodes
 Plugin URI: http://www.oik-plugins.com/oik-plugins/oik-bob-bing-wide-plugin
 Description: More lazy smart shortcodes: bw_plug, bw_page, bw_post, bob/fob bing/bong wide/hide & wow, oik and loik, wp, wpms, bp, artisteer, drupal
-Version: 1.15
+Version: 1.16
 Author: bobbingwide
 Author URI: http://www.bobbingwide.com
 License: GPL2
